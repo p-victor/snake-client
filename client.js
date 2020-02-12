d152d7a4b792e17ced4b10ed777f6104c9d0d3f1
victor poirier <poiriervictor99@gmail.com>
2020-02-12 18:03:45 +0000

    added name to connection

--- client.js
+++ client.js
@@ -2,11 +2,17 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '',
+    host: '192.168.0.103',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write( "Name: VIC")
+  })
 
   conn.on('data', (data) => {
     console.log(data);
